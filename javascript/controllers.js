d47094c0f0d03350308764b4ca3aa0ae3ff3e3bb
GilMC-Geoff <gwebb@gilmc.com>
2017-05-15 16:24:19 -0500

    Added xlsx parser function

--- javascript/controllers.js
+++ javascript/controllers.js
@@ -8,27 +8,28 @@ app.controller("FirstCtrl", ($scope) => {
 app.controller("SideCtrl", ($scope) => {
     $scope.name = "Sidebar";
 
-    //     /* set up XMLHttpRequest */
-    // var url = "test_files/formula_stress_test_ajax.xlsx";
-    // var oReq = new XMLHttpRequest();
-    // oReq.open("GET", url, true);
-    // oReq.responseType = "arraybuffer";
-    //
-    // oReq.onload = function(e) {
-    //   var arraybuffer = oReq.response;
-    //
-    //   /* convert data to binary string */
-    //   var data = new Uint8Array(arraybuffer);
-    //   var arr = new Array();
-    //   for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
-    //   var bstr = arr.join("");
-    //
-    //   /* Call XLSX */
-    //   var workbook = XLSX.read(bstr, {type:"binary"});
-    //
-    //   /* DO SOMETHING WITH workbook HERE */
-    // }
-    //
-    // oReq.send();
+    /* set up XMLHttpRequest to Parse EXCEL spreadsheet */
+    var url = "db/Book1.xlsx";
+    var oReq = new XMLHttpRequest();
+    oReq.open("GET", url, true);
+    oReq.responseType = "arraybuffer";
+
+    oReq.onload = function(e) {
+      var arraybuffer = oReq.response;
+
+      /* convert data to binary string */
+      var data = new Uint8Array(arraybuffer);
+      var arr = new Array();
+      for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+      var bstr = arr.join("");
+
+      /* Call XLSX */
+      var workbook = XLSX.read(bstr, {type:"binary"});
+
+      /* DO SOMETHING WITH workbook HERE */
+      console.log(workbook);
+    }
+
+    oReq.send();
 
 });

6eb7cc96d5b4d625a4c5da9701ef5a7f3f79a466
GilMC-Geoff <gwebb@gilmc.com>
2017-05-16 17:00:36 -0500

    Working on async for the excel data

--- javascript/controllers.js
+++ javascript/controllers.js
@@ -1,16 +1,21 @@
 var app = angular.module("Dashboard", []);
 
 app.controller("FirstCtrl", ($scope) => {
-    $scope.items = [{ name: 'Main Content Area', pdf:"pdf/ChristieCookie.pdf" }];
+  $scope.items = [{
+    name: 'Main Content Area',
+    pdf: "pdf/ChristieCookie.pdf"
+  }];
 
 });
 
 app.controller("SideCtrl", ($scope) => {
-    $scope.name = "Sidebar";
-
-    $scope.jobs = [];
+  $scope.name = "Sidebar";
+  $scope.jobs = [];
+  //$scope.master = ["test1", "test2"]
+  //console.log($scope.master);
+  const getList = function() {
     /* set up XMLHttpRequest to Parse EXCEL spreadsheet */
-    var url = "db/Book1.xlsx";
+    var url = "db/list/PROJECTLIST master.xls.xlsx";
     var oReq = new XMLHttpRequest();
     oReq.open("GET", url, true);
     oReq.responseType = "arraybuffer";
@@ -21,19 +26,67 @@ app.controller("SideCtrl", ($scope) => {
       /* convert data to binary string */
       var data = new Uint8Array(arraybuffer);
       var arr = new Array();
-      for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+      for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
       var bstr = arr.join("");
 
       /* Call XLSX */
-      var workbook = XLSX.read(bstr, {type:"binary"});
+      var workbook = XLSX.read(bstr, {
+        type: "binary"
+      });
 
       /* DO SOMETHING WITH workbook HERE */
-      let jobs = workbook;
-      console.log(workbook);
-      // $scope.jobs.push(jobs);
-      // console.log($scope.jobs[0]);
-    }
+      let jobs = workbook.Sheets["Master Project List"];
+      //console.log(workbook,jobs);
 
+      var sheet2arr = function(sheet) {
+        var result = [];
+        var row;
+        var rowNum;
+        var colNum;
+        var range = XLSX.utils.decode_range(sheet['!ref']);
+        for (rowNum = range.s.r; rowNum <= range.e.r; rowNum++) {
+          row = [];
+          for (colNum = range.s.c; colNum <= range.e.c; colNum++) {
+            var nextCell = sheet[
+              XLSX.utils.encode_cell({
+                r: rowNum,
+                c: colNum
+              })
+            ];
+            if (typeof nextCell === 'undefined') {
+              row.push(void 0);
+            } else row.push(nextCell.w);
+          }
+          result.push(row);
+        }
+        return result;
+      };
+
+      let arrToSlice = sheet2arr(jobs);
+      let newList = arrToSlice.slice(5, 700);
+      let arrayList = [];
+      let jobObj = {};
+
+      newList.forEach(function(job) {
+        jobObj.name = job[0];
+        jobObj.code = job[1];
+        jobObj.number = job[2];
+        jobObj.phase = job[3];
+        arrayList.push(jobObj);
+        arrayList.reverse();
+      });
+
+      $scope.master = arrayList;
+
+      return $scope.master;
+
+    }
     oReq.send();
+  };
+
+  getList();
+  //console.log(getList());
+//  $scope.master = getList();
+
 
 });
--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,13 @@
 let datetime = null,
-        date = null;
+  date = null;
 
-const update = function () {
-    date = moment(new Date())
-    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm a'));
+const update = function() {
+  date = moment(new Date())
+  datetime.html(date.format('dddd, MMMM Do YYYY, h:mm a'));
 };
 
-$(document).ready(function(){
-    datetime = $('#time');
-    update();
-    setInterval(update, 1000);
+$(document).ready(function() {
+  datetime = $('#time');
+  update();
+  setInterval(update, 1000);
 });
